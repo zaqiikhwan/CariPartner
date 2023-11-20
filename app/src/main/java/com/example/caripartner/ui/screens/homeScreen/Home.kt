1f32aee0f89d9bc55f16379bacdbf494ae7cca68
Muh Zaqi <zaqiikhwan22@gmail.com>
2023-11-20 20:52:37 +0700

    feat: ```bottom navbar```, add: ```dummy screen chat, partner, profile, and search```

--- app/src/main/java/com/example/caripartner/ui/screens/homeScreen/Home.kt
+++ app/src/main/java/com/example/caripartner/ui/screens/homeScreen/Home.kt
@@ -1,48 +1,171 @@
 package com.example.caripartner.ui.screens.homeScreen
 
-//import androidx.compose.material.MaterialTheme
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.caripartner.ui.screens.chatScreen.ChatScreen
 import com.example.caripartner.ui.screens.loginScreen.LoginViewModel
+import com.example.caripartner.ui.screens.partnerScreen.PartnerScreen
+import com.example.caripartner.ui.screens.profileScreen.ProfileScreen
+import com.example.caripartner.ui.screens.searchScreen.SearchScreen
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 
-@Composable
-
-fun Home(loginViewModel: LoginViewModel?){
-    val loginUiState = loginViewModel?.loginUiState
-//    println(loginUiState)
-    Text(text="This is home screen"+loginViewModel?.userId)
-
-    Button(onClick = { logout },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 100.dp, top = 200.dp, bottom = 100.dp, end = 100.dp)
-            .background(color = Color(0xFF4B4EFC), shape = RoundedCornerShape(size = 12.dp)),
-        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4B4EFC))
-    ) {
-        Text(text = "Logout",
-            fontSize = 18.sp,
-            lineHeight = 28.sp,
-            fontWeight = FontWeight(600),
-            color = Color(0xFFFFFFFF),)
-    }
+enum class BottomNavRoutes{
+    Home,
+    Search,
+    Partner,
+    Chat,
+    Profile
 }
 
-// Obtain an instance of FirebaseAuth
-//val firebaseAuth = FirebaseAuth.getInstance()
-val logout = Firebase.auth.signOut()
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Home(loginViewModel: LoginViewModel?){
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                BottomNavigationItem().bottomNavigationItems().forEachIndexed { _, navigationItem ->
+                    NavigationBarItem(
+                        selected = navigationItem.route == currentDestination?.route,
+                        label = {
+                            Text(navigationItem.label)
+                        },
+                        icon = {
+                            Icon(
+                                navigationItem.icon,
+                                contentDescription = navigationItem.label
+                            )
+                        },
+                        onClick = {
+                            navController.navigate(navigationItem.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) {paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = BottomNavRoutes.Home.name,
+            modifier = Modifier.padding(paddingValues = paddingValues)) {
+            composable(BottomNavRoutes.Home.name){
+//                Home(loginViewModel = loginViewModel)
+                Text(text="This is home screen"+loginViewModel?.userId)
+
+                Button(onClick = { Firebase.auth.signOut() },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 100.dp, top = 200.dp, bottom = 100.dp, end = 100.dp)
+                        .background(color = Color(0xFF4B4EFC), shape = RoundedCornerShape(size = 12.dp)),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4B4EFC))
+                ) {
+                    Text(text = "Logout",
+                        fontSize = 18.sp,
+                        lineHeight = 28.sp,
+                        fontWeight = FontWeight(600),
+                        color = Color(0xFFFFFFFF),)
+                }
+            }
+            composable(BottomNavRoutes.Search.name) {
+                SearchScreen(
+                    navController
+                )
+            }
+            composable(BottomNavRoutes.Partner.name) {
+                PartnerScreen(
+                    navController
+                )
+            }
+            composable(BottomNavRoutes.Chat.name) {
+                ChatScreen(
+                    navController
+                )
+            }
+            composable(BottomNavRoutes.Profile.name) {
+                ProfileScreen(
+                    navController
+                )
+            }
+        }
+    }
+
+}
+
+data class BottomNavigationItem(
+    val label : String = "",
+    val icon : ImageVector = Icons.Filled.Home,
+    val route : String = ""
+) {
+    fun bottomNavigationItems() : List<BottomNavigationItem> {
+        return listOf(
+            BottomNavigationItem(
+                label = "Beranda",
+                icon = Icons.Filled.Home,
+                route = BottomNavRoutes.Home.name
+            ),
+            BottomNavigationItem(
+                label = "Lomba",
+                icon = Icons.Filled.Search,
+                route = BottomNavRoutes.Search.name
+            ),
+            BottomNavigationItem(
+                label = "Partner",
+                icon = Icons.Filled.AddCircle,
+                route = BottomNavRoutes.Partner.name
+            ),
+            BottomNavigationItem(
+                label = "Pesan",
+                icon = Icons.Filled.MailOutline,
+                route = BottomNavRoutes.Chat.name
+            ),
+            BottomNavigationItem(
+                label = "Profile",
+                icon = Icons.Filled.AccountCircle,
+                route = BottomNavRoutes.Profile.name
+            ),
+        )
+    }
+}
 
 
 
