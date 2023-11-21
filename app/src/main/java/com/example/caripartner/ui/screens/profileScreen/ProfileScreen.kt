77386b9810f615d4e4b34c5963877b2153e9f280
Abizard <abizard.03@gmail.com>
2023-11-21 19:07:26 +0700

    feat: profile-screen

--- app/src/main/java/com/example/caripartner/ui/screens/profileScreen/ProfileScreen.kt
+++ app/src/main/java/com/example/caripartner/ui/screens/profileScreen/ProfileScreen.kt
@@ -1,59 +1,317 @@
 package com.example.caripartner.ui.screens.profileScreen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LightMode
+import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.StarOutline
+import androidx.compose.material.icons.outlined.Subtitles
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.caripartner.R
-import com.example.caripartner.ui.theme.CariPartnerTheme
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
+@Preview(showBackground = true)
 @Composable
 fun ProfileScreen() {
-    CariPartnerTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Column(
+    val navController = rememberNavController()
+    val checkedState = remember {
+        mutableStateOf(false)
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row (
+            horizontalArrangement = Arrangement.spacedBy(0.dp, Alignment.CenterHorizontally),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .shadow(
+                    elevation = 8.dp,
+                    spotColor = Color(0x296481DC),
+                    ambientColor = Color(0x296481DC)
+                )
+                .width(390.dp)
+                .height(100.dp)
+                .padding(top = 16.dp, bottom = 16.dp)
+        ){
+            Text(
+                text = "Profile",
+                fontSize = 20.sp,
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight(600),
+                color = Color(0xFF383B46),
+                textAlign = TextAlign.Center
+            )
+        }
+
+        Spacer(
+            modifier = Modifier
+                .padding(top = 24.dp)
+        )
+
+        //Card Profile
+        Row (
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .width(358.dp)
+                .height(100.dp)
+                .background(color = Color(0xFFF5F5FF), shape = RoundedCornerShape(size = 8.dp))
+                .padding(start = 16.dp, bottom = 16.dp, end = 16.dp, top = 16.dp)
+                .align(Alignment.CenterHorizontally)
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.muhzaqi),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(15.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            )  {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp)
-                        .padding(horizontal = 15.dp, vertical = 10.dp)
-                        .clip(MaterialTheme.shapes.large)
-                ) {
+                    .width(68.dp)
+                    .height(68.dp)
+                    .background(
+                        MaterialTheme.colorScheme.primary,
+                        shape = RoundedCornerShape(size = 8.dp)
+                    )
+            )
+            Column (
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(40.dp)
+                    .padding(start = 24.dp)
+            ) {
+                Row (
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Column (
+                        horizontalAlignment = Alignment.Start,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Muh Zaqi",
+                            fontSize = 14.sp,
+                            lineHeight = 18.sp,
+                            fontWeight = FontWeight(600),
+                            color = Color(0xFF101828),
+                            modifier = Modifier
+                                .width(72.dp)
+                                .height(18.dp)
+                        )
+                        Text(
+                            text = "UI/UX",
+                            fontSize = 12.sp,
+                            lineHeight = 18.sp,
+                            fontWeight = FontWeight(600),
+                            color = Color(0xFF1E22FB)
+                        )
+                    }
+                }
+            }
+            Row (
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.Top,
+                modifier = Modifier
+                    .padding(bottom = 20.dp)
+            ){
+                if(checkedState.value){
                     Image(
-                        painter = painterResource(R.drawable.three),
-                        contentDescription = "profile_screen_bg",
-                        contentScale = ContentScale.Crop
+                        painter = painterResource(id = R.drawable.bersedia),
+                        contentDescription = "",
+                        contentScale = ContentScale.None,
+                        modifier = Modifier
+                            .width(12.dp)
+                            .height(18.dp)
+                    )
+                }else{
+                    Image(
+                        painter = painterResource(id = R.drawable.tidak_bersedia),
+                        contentDescription = "",
+                        contentScale = ContentScale.None,
+                        modifier = Modifier
+                            .width(12.dp)
+                            .height(18.dp)
                     )
                 }
                 Text(
-                    "Profile Screen",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.padding(vertical = 20.dp)
+                    text = if(checkedState.value) "Bersedia" else "Tidak Bersedia",
+                    fontSize = 11.sp,
+                    lineHeight = 18.sp,
+                    fontWeight = FontWeight(600),
+                    color = if(checkedState.value) Color(0xFF4B4EFC) else Color(0xFFB42318),
+                    textAlign = TextAlign.Start,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(18.dp)
                 )
             }
+        }
+        Text(
+            text = "Personal",
+            fontSize = 20.sp,
+            lineHeight = 30.sp,
+            fontWeight = FontWeight(600),
+            color = Color(0xFF101828),
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(start = 20.dp, top = 16.dp, bottom = 16.dp)
+        )
+
+        //Personal Menu
+        Column (
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier
+                .width(357.dp)
+                .height(57.dp)
+                .background(color = Color(0xFFF2F4F7))
+                .padding(start = 12.dp, top = 16.dp, end = 12.dp, bottom = 16.dp)
+                .align(Alignment.CenterHorizontally)
+        ){
+            Row (
+                horizontalArrangement = Arrangement.spacedBy(120.dp)
+            ){
+                Row {
+                    Icon(
+                        Icons.Default.LightMode,
+                        contentDescription = "",
+                        modifier = Modifier
+                            .width(24.dp)
+                            .height(24.dp)
+                    )
+                    Text(
+                        text = "Status Kesediaan",
+                        fontSize = 16.sp,
+                        lineHeight = 24.sp,
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF101828),
+                        modifier = Modifier
+                            .padding(start = 12.dp)
+                    )
+                }
+
+                Switch(
+                    checked = checkedState.value,
+                    onCheckedChange = {checkedState.value = it},
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = Color.White,
+                        checkedTrackColor = Color(0xFF4B4EFC),
+                        uncheckedThumbColor = Color.White,
+                        uncheckedTrackColor = Color.Gray
+                    ),
+                    modifier = Modifier
+                        .width(48.dp)
+                        .height(25.dp)
+                )
+            }
+        }
+        ClickablePersonal(Icons.Outlined.Person,"Biodata", "biodata", navController)
+        ClickablePersonal(Icons.Outlined.StarOutline,"Bidang Dikuasai", "bidangDikuasai", navController)
+        ClickablePersonal(Icons.Outlined.FavoriteBorder, "Keminatan", "keminatan", navController)
+        ClickablePersonal(Icons.Outlined.Subtitles,"Penghargaan", "penghargaan", navController)
+
+        Text(
+            text = "Pengaturan",
+            fontSize = 20.sp,
+            lineHeight = 30.sp,
+            fontWeight = FontWeight(600),
+            color = Color(0xFF101828),
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(start = 20.dp, top = 16.dp, bottom = 16.dp)
+        )
+
+        Button(
+            onClick = { Firebase.auth.signOut() },
+            colors = ButtonDefaults.buttonColors(Color(0xFFF04438)),
+            modifier = Modifier
+                .width(357.dp)
+                .height(56.dp)
+                .background(color = Color(0xFFF04438), shape = RoundedCornerShape(size = 12.dp))
+
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(imageVector = Icons.Default.Logout, contentDescription = "")
+                Text(
+                    text = "Keluar",
+                    fontSize = 16.sp,
+                    lineHeight = 24.sp,
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFFFFFFFF),
+                    modifier = Modifier
+                        .padding(start = 12.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ClickablePersonal(icon: ImageVector, label: String, destination: String, navController: NavController) {
+    Column(
+        modifier = Modifier
+            .width(357.dp)
+            .height(57.dp)
+            .background(color = Color(0xFFF2F4F7))
+            .padding(start = 12.dp, top = 16.dp, end = 12.dp, bottom = 16.dp)
+            .clickable { navController.navigate(destination) }
+    ) {
+        Row {
+            Icon(
+                icon,
+                contentDescription = ""
+            )
+            Text(
+                text = label,
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+                fontWeight = FontWeight(400),
+                color = Color(0xFF101828),
+                modifier = Modifier
+                    .padding(start = 12.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
         }
     }
 }
