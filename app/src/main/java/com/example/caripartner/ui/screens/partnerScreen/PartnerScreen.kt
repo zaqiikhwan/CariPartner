bc9928b264bec754c4de1cdc13213ab44d546103
Muhammad Masyhur <muhammadmasyhur2@gmail.com>
2023-11-21 22:21:01 +0700

    feat: 80% partnerscreen ya anjing

--- app/src/main/java/com/example/caripartner/ui/screens/partnerScreen/PartnerScreen.kt
+++ app/src/main/java/com/example/caripartner/ui/screens/partnerScreen/PartnerScreen.kt
@@ -1,28 +1,56 @@
 package com.example.caripartner.ui.screens.partnerScreen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.caripartner.R
+import com.example.caripartner.data.model.User
+import com.example.caripartner.data.repository.UserRepository
 import com.example.caripartner.ui.theme.CariPartnerTheme
 
 @Composable
-fun PartnerScreen() {
+fun PartnerScreen(userRepository: UserRepository, user: Int) {
     CariPartnerTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
@@ -30,31 +58,230 @@ fun PartnerScreen() {
         ) {
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(15.dp),
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 24.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
+                Text(
+                    "Cari Partner",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                SearchBar(
+                    onSearchTextChanged = { searchText ->
+                        // Lakukan sesuatu dengan nilai searchText (misalnya, filter data berdasarkan pencarian)
+                    },
+                    placeholder = "Ketik untuk mencari partner"
+                )
                 Box(
                     modifier = Modifier
+                        .size(width = 358.dp, height = 50.dp)
+                        .background(
+                            Color.Blue,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                ) {
+                    Row {
+                        Text(
+                            text = "Rekomedasi partner lomba",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = Color.White,
+                            modifier = Modifier
+                                .padding(start = 12.dp, top = 16.dp, bottom = 16.dp)
+                        )
+                        Button(
+                            onClick = { /*TODO*/ },
+                            colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                            shape = RoundedCornerShape(8.dp),
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(top = 4.dp, bottom = 4.dp, start = 24.dp, end = 16.dp)
+                        ) {
+                            Text(
+                                text = "Coba Sekarang",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = Color.Blue
+                            )
+                        }
+                    }
+                }
+                Row (
+                    modifier = Modifier
+                        .padding(start = 16.dp, top = 24.dp, bottom = 16.dp)
+                        .size(width = 358.dp, height = 36.dp),
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Filter:",
+                        textAlign = TextAlign.Start,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(end = 4.dp)
+                    )
+                    Button(
+                        onClick = { /*TODO*/ },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                        modifier = Modifier
+                            .padding(vertical = 0.dp, horizontal = 4.dp)
+                            .border(
+                                width = 1.dp,
+                                color = Color.Blue,
+                                shape = RoundedCornerShape(6.dp)
+                            )
+                            .fillMaxHeight()
+                    ) {
+                        Text(
+                            text = "Prodi",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = Color.Blue
+                        )
+                    }
+                    Button(
+                        onClick = { /*TODO*/ },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                        modifier = Modifier
+                            .padding(vertical = 0.dp, horizontal = 4.dp)
+                            .border(
+                                width = 1.dp,
+                                color = Color.Blue,
+                                shape = RoundedCornerShape(6.dp)
+                            )
+                            .fillMaxHeight()
+                    ) {
+                        Text(
+                            text = "Bidang",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = Color.Blue
+                        )
+                    }
+                    Button(
+                        onClick = { /*TODO*/ },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                        modifier = Modifier
+                            .padding(vertical = 0.dp, horizontal = 4.dp)
+                            .border(
+                                width = 1.dp,
+                                color = Color.Blue,
+                                shape = RoundedCornerShape(6.dp)
+                            )
+                            .fillMaxHeight()
+                    ) {
+                        Text(
+                            text = "Bersedia",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = Color.Blue
+                        )
+                    }
+                }
+                Row(
+                    modifier = Modifier
                         .fillMaxWidth()
-                        .height(200.dp)
-                        .padding(horizontal = 15.dp, vertical = 10.dp)
-                        .clip(MaterialTheme.shapes.large)
+                        .padding(start = 16.dp, top = 16.dp, end = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
                     Image(
-                        painter = painterResource(R.drawable.images),
-                        contentDescription = "partner_screen_bg",
+                        painter = painterResource(id = R.drawable.muhzaqi),
+                        contentDescription = "",
                         contentScale = ContentScale.Crop,
-                        modifier = Modifier.fillMaxSize()
+                        modifier = Modifier
+                            .width(68.dp)
+                            .height(68.dp)
+                            .background(
+                                MaterialTheme.colorScheme.primary,
+                                shape = RoundedCornerShape(8.dp)
+                            )
                     )
+                    Column (
+                        horizontalAlignment = Alignment.Start,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .padding(start = 10.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Albert Albar",
+                            fontSize = 14.sp,
+                            lineHeight = 18.sp,
+                            fontWeight = FontWeight(600),
+                            color = Color(0xFF101828)
+                        )
+                        Text(
+                            text = "Sistem Informasi",
+                            fontSize = 12.sp,
+                            lineHeight = 18.sp,
+                            fontWeight = FontWeight(600),
+                            color = Color(0xFF667085)
+                        )
+                        Text(
+                            text = "Business Plan",
+                            fontSize = 12.sp,
+                            lineHeight = 18.sp,
+                            fontWeight = FontWeight(600),
+                            color = Color(0xFF1E22FB)
+                        )
+                    }
+                    //Bersedia / Tidak bersedia
                 }
-                Text(
-                    "Partner Screen",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.padding(vertical = 20.dp)
-                )
             }
         }
+    }
+}
+
+@Composable
+fun SearchBar(
+    onSearchTextChanged: (String) -> Unit,
+    placeholder: String = "Ketik untuk mencari partner"
+) {
+    var text by remember { mutableStateOf(TextFieldValue()) }
+
+    Box(
+        modifier = Modifier
+            .size(width = 390.dp, height = 44.dp)
+            .padding(horizontal = 16.dp, vertical = 24.dp)
+            .border(
+                width = 1.dp,
+                color = Color.Gray,
+                shape = RoundedCornerShape(6.dp)
+            )
+    ) {
+        BasicTextField(
+            value = text,
+            onValueChange = {
+                text = it
+                onSearchTextChanged(it.text)
+            },
+            singleLine = true,
+            textStyle = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier
+                .width(358.dp)
+                .height(44.dp),
+            decorationBox = { innerTextField ->
+                if(text.text.isEmpty()) {
+                    Text(
+                        text = placeholder,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.Gray,
+                        modifier = Modifier.padding(horizontal = 36.dp, vertical = 12.dp)
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .padding(8.dp),
+                    content = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search Icon",
+                            tint = Color.Gray,
+                            modifier = Modifier.size(20.dp)
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        innerTextField()
+                    }
+                )
+            }
+        )
     }
 }
--- app/src/main/java/com/example/caripartner/ui/theme/Color.kt
+++ app/src/main/java/com/example/caripartner/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF101828)
+val Gray = Color(0xFFD0D5DD)
+val White = Color(0XFFFFFFFF)
+
+val Blue = Color(0xFF4B4EFC)
